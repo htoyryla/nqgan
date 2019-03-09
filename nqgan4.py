d296bcf35aafef81bf47434a5603666061a03d85
Hannu Töyrylä <hannu.toyryla@gmail.com>
2019-03-09 12:11:22 +0200

    corrected grayscale handling

--- nqgan4.py
+++ nqgan4.py
@@ -224,7 +224,10 @@ def bw2rgb(im, out=True):
    #print(im.shape) 
    return im
 
-
+def rgb2bw(rgb):
+    r, g, b = rgb[0,:,:], rgb[1,:,:], rgb[2,:,:]
+    gray = 0.2989 * r + 0.5870 * g + 0.1140 * b
+    return gray
 
 # create models
 
